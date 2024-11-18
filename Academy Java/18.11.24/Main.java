19ac75573c67733e7428747b24ab203f9287dbc2
Di Zenzo Carmine Federico <114579198+dizenzoc@users.noreply.github.com>
2024-11-18 17:30:51 +0100

    Delete Academy Java/18.11.24 directory

--- Academy Java/18.11.24/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Ciao Mondo");
-        System.out.println("Questo e' un esempio di classe Java!");
-    }
-}
